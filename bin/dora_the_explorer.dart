4623b18890a2847fcdd406656a565955d84b4b9b
anquitas <learningcurve2048@gmail.com>
2025-03-24 20:22:25 +0300

    first commit -A

---
+++ bin/dora_the_explorer.dart
@@ -0,0 +1,5 @@
+import 'package:dora_the_explorer/dora_the_explorer.dart' as dora_the_explorer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dora_the_explorer.calculate()}!');
+}
---
+++ test/dora_the_explorer_test.dart
@@ -0,0 +1,8 @@
+import 'package:dora_the_explorer/dora_the_explorer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
